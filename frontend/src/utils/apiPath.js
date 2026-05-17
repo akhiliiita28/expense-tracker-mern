e042faddf99b959a5564aa4c88c965ab47c8a64d
Akhil S <akhiliiita28@gmail.com>
2026-05-17 18:10:35 +0530

    frontend env file and api path update

--- frontend/src/utils/apiPath.js
+++ frontend/src/utils/apiPath.js
@@ -1,4 +1,7 @@
-export const BASE_URL="http://localhost:8000";
+const rawBaseUrl =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+
+export const BASE_URL = rawBaseUrl.replace(/\/$/, "");
 
 //utils/apiPath.js
 export const API_PATHS = {
